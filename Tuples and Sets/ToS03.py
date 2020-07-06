31c67d9e9c7061aea1676870dbd38cedb15db8c9
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2020-07-06 16:20:15 +0530

    Create ToS03.py

---
+++ Tuples and Sets/ToS03.py
@@ -0,0 +1,23 @@
+"""
+Use python set to check if string is panagram, i.e., a sentence that uses every letter in the alphabet.
+"""
+
+import string
+print("Burla Sai Teja - Part B - 3")
+def pangram(str): 
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    for i in alphabets: 
+        if i not in str.lower(): 
+            return False
+    return True
+s = input("Enter the string : ")
+if(pangram(s) == True): 
+    print("Yes it is Pangram!!!") 
+else: 
+    print("No it is not Pangram!!!")
+    
+"""
+Output:
+Enter the string : Sixty zippers were quickly picked from the woven jute bag
+Yes it is Pangram!!!
+"""
