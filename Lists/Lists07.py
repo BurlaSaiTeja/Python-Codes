8dfd5562c556f6b5cfb28ea4d0316c28759b19cc
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2020-07-06 16:40:47 +0530

    Create Lists07.py

---
+++ Lists/Lists07.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program to append a list to the second list. 
+"""
+
+list1=[1,2,3,4,5]
+list2=[6,7,8,9,10]
+list1.extend(list2)
+print(list1)
+
+"""
+Output:
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
