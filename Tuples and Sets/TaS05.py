7a334d929d45ae5d45152bb689f2b9aecb9d0bdc
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2020-07-06 16:25:18 +0530

    Create TaS05.py

---
+++ Tuples and Sets/TaS05.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program to find maximum and the minimum value in a set.
+"""
+
+s = set([2,65,34,15,4,95,77]) 
+print("Minimum in the set is :",min(s))
+print("Maximum in the set is :",max(s))
+
+"""
+Output:
+Minimum in the set is : 2
+Maximum in the set is : 95
+"""
