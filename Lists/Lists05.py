49ce374c71aa6ea701f5e3ed911a5f244e827418
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2020-07-06 16:38:46 +0530

    Create Lists05.py

---
+++ Lists/Lists05.py
@@ -0,0 +1,17 @@
+"""
+Write a Python program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included).
+"""
+
+def sqval():
+    l=list()
+    for i in range(1,31):
+        l.append(i**2)
+    print(l[:5])
+    print(l[-5:])
+sqval()
+
+"""
+Output:
+[1, 4, 9, 16, 25]
+[676, 729, 784, 841, 900]
+"""
