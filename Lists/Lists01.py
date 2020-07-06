c043a574fe54f3960d29f079a53bcc7752783446
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2020-07-06 16:33:18 +0530

    Create Lists01.py

---
+++ Lists/Lists01.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to get the largest number from a list.
+"""
+
+lists = [10, 20, 99, 45, 4]  
+lists.sort()  
+print("Largest element is = ", lists[-1])
+
+"""
+Output:
+Largest element is =  99
+"""
